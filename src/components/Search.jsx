c64c8d7c6d50eaf3aa3e79b5f45fdc35e278946b
jesmoo <habidmoo@gmail.com>
2021-11-24 11:28:19 -0600

    :lipstick: search
    adding browser styles

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import '../styles/components/Search.css';
 
 const Search = () => {
   return (
     <section className="container__sectionSearch">
-      <span className="sectionSearch__icon" />
+      <span className="sectionSearch__icon"/>
       <input
         className="sectionSearch__text"
         type="text"
         name="search"
         id="search"
+        placeholder="Search for a country..."
       />
     </section>
   );
