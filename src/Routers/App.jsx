228e2d53f7f86aaed375126107bd192db27cc396
jesmoo <habidmoo@gmail.com>
2021-11-30 12:01:46 -0600

    :construction: routes
    the routes for component change have been added

---
+++ src/Routers/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from '../containers/Layout';
+import Main from '../components/Main';
+import Details from '../components/Details';
+import NotFound from '../components/NotFound';
+import '../styles/App.css';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route exact path="/" element={<Main />} />
+          <Route exact path="/details" element={<Details />} />
+          <Route element={<NotFound />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+};
+
+export default App;
