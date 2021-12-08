63bfb2b0879c3227eca7ab1300c5e16c33cafb91
jesmoo <habidmoo@gmail.com>
2021-12-08 12:31:24 -0600

    :lipstick: details
    added the queris to make it responsive

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -3,6 +3,7 @@ import { Link, useParams } from 'react-router-dom';
 import TextContain from './TextContain';
 import useOneData from '../Hooks/useOneData';
 import '../styles/components/Details.css';
+import '../styles/breakpoints/detailsQueris.css';
 
 const Details = () => {
   const { name } = useParams();
@@ -25,23 +26,25 @@ const Details = () => {
             className="main__flag"
           />
           <h1 className="main__country">{data[0].name}</h1>
-          <TextContain title="Native Name" contain={data[0].nativeName} />
-          <TextContain title="Capital" contain={data[0].capital} />
-          <TextContain title="Region" contain={data[0].region} />
-          <TextContain title="Subregion" contain={data[0].subregion} />
-          <TextContain title="Timezones" contain={data[0].timezones[0]} />
-          <TextContain
-            title="Currencies"
-            contain={data[0].currencies[0].code}
-          />
-          <TextContain
-            title="Population"
-            contain={numberFormat.format(data[0].population)}
-          />
-          <TextContain
-            title="Languages"
-            contain={`${data[0].languages.map((item) => ` ${item.name}`)}`}
-          />
+          <section className="Maincontain__text">
+            <TextContain title="Native Name" contain={data[0].nativeName} />
+            <TextContain title="Capital" contain={data[0].capital} />
+            <TextContain title="Region" contain={data[0].region} />
+            <TextContain title="Subregion" contain={data[0].subregion} />
+            <TextContain title="Timezones" contain={data[0].timezones[0]} />
+            <TextContain
+              title="Currencies"
+              contain={data[0].currencies[0].code}
+            />
+            <TextContain
+              title="Population"
+              contain={numberFormat.format(data[0].population)}
+            />
+            <TextContain
+              title="Languages"
+              contain={`${data[0].languages.map((item) => ` ${item.name}`)}`}
+            />
+          </section>
         </>
       )}
     </main>
