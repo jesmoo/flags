863a728383030b6c6dc5e88c0339d8904f9bdd60
jesmoo <habidmoo@gmail.com>
2021-12-09 12:29:21 -0600

    :lipstick: back
    added the back to main button

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/components/notFound.css';
 
 const NotFound = () => {
   return (
     <main className="container__main-notFound">
+      <Link to="/" className="main__back btnNotfound">
+        <span className="back__icon" />
+        <p>Back</p>
+      </Link>
       <h1>Not found</h1>
       <span className="main-notFound__icon" />
     </main>
